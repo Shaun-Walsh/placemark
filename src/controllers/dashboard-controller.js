5ac37a97754ca0f0eab19d39354f10ce81957afc
Shaun Walsh <shaunwalsh24@gmail.com>
2025-02-21 14:54:00 +0000

    created accounts controller, modified dashboard controler, updated webroutes, and server.js

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,8 +1,24 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
-    index: {
-      handler: async function (request, h) {
-        return h.view("main");
-      },
+  index: {
+    handler: async function (request, h) {
+      const venueTypes = await db.venueTypeStore.getAllVenueTypes();
+      const viewData = {
+        title: "Placemark Dashboard",
+        venueTypes: venueTypes,
+      };
+      return h.view("dashboard-view", viewData);
     },
-  };
-  
+  },
+
+  addVenueType: {
+    handler: async function (request, h) {
+      const newVenueType = {
+        title: request.payload.title,
+      };
+      await db.venueTypeStore.addVenueType(newVenueType);
+      return h.redirect("/dashboard");
+    },
+  },
+};
