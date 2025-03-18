534573b9f5bb8b198f339f5b5937fd465019f360
Shaun Walsh <shaunwalsh24@gmail.com>
2025-03-18 21:09:25 +0000

    created rest of venue type api and relevant tests

--- src/api/venueType-api.js
+++ src/api/venueType-api.js
@@ -1,28 +1,65 @@
 import Boom from "@hapi/boom";
+import { venueTypeSpec } from "../models/joi-schemas.js"
 import { db } from "../models/db.js";
 
 export const venueTypeApi = {
   find: {
     auth: false,
     handler: async function (request, h) {
-    },
-  },
-
+        try {
+            const venueTypes = await db.venueTypeStore.getAllVenueTypes();
+            return venueTypes;
+          } catch (err) {
+            return Boom.serverUnavailable("Database Error");
+          }
+        },
+      },
+  
   findOne: {
     auth: false,
     async handler(request) {
+      try {
+        const venueType = await db.venueTypeStore.getVenueTypeById(request.params.id);
+        if (!venueType) {
+          return Boom.notFound("No Venue Type with this id");
+        }
+        return venueType;
+      } catch (err) {
+        return Boom.serverUnavailable("No Venue Type with this id");
+      }
     },
   },
+
 
   create: {
     auth: false,
     handler: async function (request, h) {
+      try {
+        const venueType = request.payload;
+        const newVenueType = await db.venueTypeStore.addVenueType(venueType);
+        if (newVenueType) {
+          return h.response(newVenueType).code(201);
+        }
+        return Boom.badImplementation("error creating Venue Type");
+      } catch (err) {
+        return Boom.serverUnavailable("Database Error");
+      }
     },
   },
 
   deleteOne: {
     auth: false,
     handler: async function (request, h) {
+      try {
+        const venueType = await db.venueTypeStore.getVenueTypeById(request.params.id);
+        if (!venueType) {
+          return Boom.notFound("No Venue Type with this id");
+        }
+        await db.venueTypeStore.deleteVenueTypeById(venueType._id);
+        return h.response().code(204);
+      } catch (err) {
+        return Boom.serverUnavailable("No Venue Type with this id");
+      }
     },
   },
 
--- test/api/venueType-api-test.js
+++ test/api/venueType-api-test.js
@@ -20,14 +20,42 @@ suite("Venue Type API tests", () => {
   teardown(async () => {});
 
   test("create Venue Type", async () => {
+    const returnedVenueType = await placemarkService.createVenueType(pub);
+    assert.isNotNull(returnedVenueType);
+    assertSubset(pub, returnedVenueType);
   });
 
   test("delete a Venue Type", async () => {
+    const venueType = await placemarkService.createVenueType(pub);
+    const response = await placemarkService.deleteVenueType(venueType._id);
+    assert.equal(response.status, 204);
+    try {
+      const returnedVenueType = await placemarkService.getVenueType(venueType.id);
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No Venue Type with this id", "Incorrect Response Message");
+    }
   });
 
   test("create multiple Venue Types", async () => {
-  });
+        for (let i = 0; i < testVenueTypes.length; i += 1) {
+          testVenueTypes[i].userid = user._id;
+          // eslint-disable-next-line no-await-in-loop
+          await placemarkService.createVenueType(testVenueTypes[i]);
+        }
+        let returnedLists = await placemarkService.getAllVenueTypes();
+        assert.equal(returnedLists.length, testVenueTypes.length);
+        await placemarkService.deleteAllVenueTypes();
+        returnedLists = await placemarkService.getAllVenueTypes();
+        assert.equal(returnedLists.length, 0);
+      });
 
   test("remove non-existant Venue Type", async () => {
+    try {
+      const response = await placemarkService.deleteVenueType("not an id");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No Venue Type with this id", "Incorrect Response Message");
+    }
   });
 });
